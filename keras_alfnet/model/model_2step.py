c004d874f76789854ea4222ed09b7d1750a7dc53
liuwei16 <liuwei16@nudt.edu.cn>
2018-11-22 18:25:10 +0800

    update

--- keras_alfnet/model/model_2step.py
+++ keras_alfnet/model/model_2step.py
@@ -24,9 +24,12 @@ class Model_2step(Base_model):
 		self.optimizer = Adam(lr=opt.init_lr)
 		print 'Initializing the {}'.format(self.name())
 
-	def creat_model(self,opt,train_data, phase='train'):
-		Base_model.create_base_model(self, opt,train_data, phase=phase)
+	def creat_model(self,opt,train_data, phase='train', wei_mov_ave = False):
+		Base_model.create_base_model(self, opt,train_data, phase=phase, wei_mov_ave = wei_mov_ave)
 		alf1, alf2 = model_alf.create_alf(self.base_layers, self.num_anchors, trainable=True, steps=2)
+		if wei_mov_ave:
+			alf1_tea, alf2_tea = model_alf.create_alf(self.base_layers_tea, self.num_anchors, trainable=True, steps=2)
+			self.model_tea = Model(self.img_input, alf1_tea + alf2_tea)
 		if phase=='train':
 			self.model_1st = Model(self.img_input, alf1)
 			self.model_2nd = Model(self.img_input, alf2)
@@ -89,6 +92,84 @@ class Model_2step(Base_model):
 							print('Total loss decreased from {} to {}, saving weights'.format(self.best_loss, total_loss))
 							self.best_loss = total_loss
 						self.model_all.save_weights(
+							os.path.join(out_path, 'resnet_e{}_l{}.hdf5'.format(epoch_num + 1 + self.add_epoch, total_loss)))
+						break
+				except Exception as e:
+					print ('Exception: {}'.format(e))
+					continue
+			records = np.concatenate((np.asarray(self.total_loss_r).reshape((-1, 1)),
+									  np.asarray(self.cls_loss_r1).reshape((-1, 1)),
+									  np.asarray(self.regr_loss_r1).reshape((-1, 1)),
+									  np.asarray(self.cls_loss_r2).reshape((-1, 1)),
+									  np.asarray(self.regr_loss_r2).reshape((-1, 1))),
+									 axis=-1)
+			np.savetxt(os.path.join(out_path, 'records.txt'), np.array(records), fmt='%.6f')
+		print('Training complete, exiting.')
+
+	def train_model_wma(self,opt, weight_path, out_path):
+		self.model_all.load_weights(weight_path, by_name=True)
+		self.model_tea.load_weights(weight_path, by_name=True)
+		print 'load weights from {}'.format(weight_path)
+		iter_num = 0
+		start_time = time.time()
+		for epoch_num in range(self.num_epochs):
+			progbar = generic_utils.Progbar(self.epoch_length)
+			print('Epoch {}/{}'.format(epoch_num + 1 + self.add_epoch, self.num_epochs + self.add_epoch))
+			while True:
+				try:
+					X, Y, img_data = next(self.data_gen_train)
+					loss_s1 = self.model_1st.train_on_batch(X, Y)
+					self.losses[iter_num, 0] = loss_s1[1]
+					self.losses[iter_num, 1] = loss_s1[2]
+					pred1 = self.model_1st.predict_on_batch(X)
+					# Y2 = bbox_process.get_target_1st(self.anchors, pred1[1], img_data, opt,
+					# 								 igthre=opt.ig_overlap, posthre=opt.pos_overlap_step2,
+					# 								 negthre=opt.neg_overlap_step2)
+					Y2 = bbox_process.get_target_1st_posfirst(self.anchors, pred1[1], img_data, opt,
+													 igthre=opt.ig_overlap, posthre=opt.pos_overlap_step2,
+													 negthre=opt.neg_overlap_step2)
+					loss_s2 = self.model_2nd.train_on_batch(X, Y2)
+					self.losses[iter_num, 2] = loss_s2[1]
+					self.losses[iter_num, 3] = loss_s2[2]
+					# apply weight moving average
+					for l in self.model_tea.layers:
+						weights_tea = l.get_weights()
+						if len(weights_tea) > 0:
+							weights_stu = self.model_all.get_layer(name=l.name).get_weights()
+							weights_tea = [opt.alpha * w_tea + (1 - opt.alpha) * w_stu for (w_tea, w_stu) in
+										   zip(weights_tea, weights_stu)]
+							l.set_weights(weights_tea)
+
+					iter_num += 1
+					if iter_num % 20 == 0:
+						progbar.update(iter_num,
+									   [('cls1', np.mean(self.losses[:iter_num, 0])),
+										('regr1', np.mean(self.losses[:iter_num, 1])),
+										('cls2', np.mean(self.losses[:iter_num, 2])),
+										('regr2', np.mean(self.losses[:iter_num, 3]))])
+					if iter_num == self.epoch_length:
+						cls_loss1 = np.mean(self.losses[:, 0])
+						regr_loss1 = np.mean(self.losses[:, 1])
+						cls_loss2 = np.mean(self.losses[:, 2])
+						regr_loss2 = np.mean(self.losses[:, 3])
+						total_loss = cls_loss1 + regr_loss1 + cls_loss2 + regr_loss2
+
+						self.total_loss_r.append(total_loss)
+						self.cls_loss_r1.append(cls_loss1)
+						self.regr_loss_r1.append(regr_loss1)
+						self.cls_loss_r2.append(cls_loss2)
+						self.regr_loss_r2.append(regr_loss2)
+
+						print('Total loss: {}'.format(total_loss))
+						print('Elapsed time: {}'.format(time.time() - start_time))
+
+						iter_num = 0
+						start_time = time.time()
+
+						if total_loss < self.best_loss:
+							print('Total loss decreased from {} to {}, saving weights'.format(self.best_loss, total_loss))
+							self.best_loss = total_loss
+						self.model_tea.save_weights(
 							os.path.join(out_path, 'resnet_e{}_l{}.hdf5'.format(epoch_num + 1 + self.add_epoch, total_loss)))
 						break
 				except Exception as e:
