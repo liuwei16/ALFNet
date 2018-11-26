cd92ac57e4ad9791f45680793a85d6c18bd1c9b3
liuwei16 <liuwei16@nudt.edu.cn>
2018-11-26 15:01:43 +0800

    basemodel

--- keras_alfnet/model/base_model.py
+++ keras_alfnet/model/base_model.py
@@ -15,7 +15,7 @@ class Base_model():
 		self.total_loss_r =[]
 		self.best_loss = 0
 
-	def create_base_model(self,opt, train_data, phase='train'):
+	def create_base_model(self,opt, train_data, phase='train',wei_mov_ave = False):
 		if opt.network == 'resnet50':
 			from . import resnet50 as nn
 		elif opt.network == 'mobilenet':
@@ -25,6 +25,8 @@ class Base_model():
 		# define the base network
 		self.img_input = Input(shape=(opt.random_crop[0], opt.random_crop[1], 3))
 		self.base_layers, feat_map_sizes = nn.nn_base(self.img_input, trainable=True)
+		if wei_mov_ave:
+			self.base_layers_tea, _ = nn.nn_base(self.img_input, trainable=True)
 		# get default anchors and define data generator
 		self.anchors, self.num_anchors = data_generators.get_anchors(img_height=opt.random_crop[0], img_width=opt.random_crop[1],
 														   feat_map_sizes=feat_map_sizes.astype(np.int),
@@ -34,4 +36,5 @@ class Base_model():
 			self.data_gen_train = data_generators.get_target(self.anchors, train_data, opt, batchsize=self.batchsize, net='2step',
 													igthre=opt.ig_overlap, posthre=opt.pos_overlap_step1,
 													negthre=opt.neg_overlap_step1)
+
 
